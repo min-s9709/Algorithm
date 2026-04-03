e8dc0a6de45408117d195440c725ebff8b73e33e
MinSung Kim <78842816+min-s9709@users.noreply.github.com>
2026-04-03 17:18:53 +0900

    [level 2] Title: 소수 찾기, Time: 11.27 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,32 @@
+function solution(numbers) {
+    const nums = numbers.split("");
+    const set = new Set();
+    
+    function permute(arr, current) {
+        if(current.length > 0) {
+            set.add(Number(current.join('')));
+        }
+        
+        for(let i = 0; i < arr.length; i++) {
+            const rest = [...arr.slice(0,i), ...arr.slice(i+1)];
+            permute(rest, [...current, arr[i]])
+        }
+    }
+    
+    permute(nums, []);
+    
+    function isPrime(n) {
+        if(n < 2) return false;
+        for(let i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i === 0) return false;
+        }
+        return true
+    }
+    
+    let count = 0;
+    for(const num of set) {
+        if(isPrime(num)) count++;
+    }
+    
+    return count;
+}
